342f291cfc1304a06d8004bbd3390d90d6a528d5
JONGTAE02 <jongxo0677@kyonggi.ac.kr>
2026-05-09 17:29:47 +0900

    refactor: 채팅(Chat) 도메인 관련 패키지 및 기능 전체 삭제 - 사용하지 않는 ChatRoom, ChatMessage 엔티티 및 관련 로직 제거

--- src/main/java/com/kgu/life_watch/domain/chat/entity/RoomStatus.java
+++
@@ -1,6 +0,0 @@
-package com.kgu.life_watch.domain.chat.entity;
-
-public enum RoomStatus {
-  ACTIVATE,
-  DEACTIVATE // 활성화, 비활성화
-}
--- src/main/java/com/kgu/life_watch/domain/chat/repository/ChatRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kgu.life_watch.domain.chat.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kgu.life_watch.domain.chat.entity.ChatRoom;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {}
